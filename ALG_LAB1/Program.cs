edffd650260858ee74b41803d033e00e2df9af24
PryseOlg <oleg.kuts.02@mail.ru>
2021-09-26 23:17:24 +0500

    Adding TimSort

--- ALG_LAB1/Program.cs
+++ ALG_LAB1/Program.cs
@@ -31,14 +31,13 @@ namespace ALG_LAB1 {
                     for (int j = 1; j <= i; j++) {
                         sum += j;
                     }
-
                     stopwatch.Stop();
                     string time = (stopwatch.ElapsedTicks).ToString();
                     File.AppendAllText(path2, time + ";");
                 }
             }
 
-            // I-3 Задание "Произведение элементов" ?
+            // I-3 Алгоритм "Произведение элементов" ?
             static void DoTaskI3() {
                 string path3 = @"..\..\Data3.csv";
                 for (int i = 1; i < 2000; i++) {
@@ -48,14 +47,16 @@ namespace ALG_LAB1 {
                     for (int j = 1; j <= i; j++) {
                         multiplic = multiplic * j;
                     }
-
                     stopwatch.Stop();
                     string time = (stopwatch.ElapsedTicks).ToString();
                     File.AppendAllText(path3, time + ";");
                 }
             }
+            
+            // I-4 Алгоритм "Хуйня какая-то" -
+            // Как это сделать????
 
-            // I-5 Алгоритм "Сортировка пузырьком" +
+            // I-5 Алгоритм "Bubble sort" +
             static void DoTaskI5() {
                 string path = @"..\..\Data5.csv";
                 int N = 2000;
@@ -90,7 +91,7 @@ namespace ALG_LAB1 {
                 }
             }
 
-            // I-6 Алгоритм "Быстрая сортировка" +
+            // I-6 Алгоритм "Quick sort" +
             static void DoTaskI6() {
                 static void Quick_Sort(int[] arr, int left, int right) {
                     if (left < right) {
@@ -145,8 +146,31 @@ namespace ALG_LAB1 {
                     File.AppendAllText(path, time + ";");
                 }
             }
-
-            // 10_1 Алгоритм "Сортировка в двумерном массиве"
+            
+            // I-7 Алгоритм "Timsort" +
+            static void DoTaskI7() {
+                int[] arr = new int[2000];
+                string path7 = @"..\..\Data7.csv";
+                
+                for (int i = 1; i < 2000; i++) {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    
+                    for (int j = 1; j <= i; j++) {
+                        Random rnd = new Random();
+                        arr[j] = rnd.Next(1, 1000);
+                    }
+                    int n = arr.Length;
+                    
+                    // Function Call
+                    GFG.timSort(arr, n);
+                    stopwatch.Stop();
+                    string time = (stopwatch.ElapsedTicks).ToString();
+                    File.AppendAllText(path7, time + ";");
+                }
+            }
+            
+            // III-Oleg Алгоритм "Сортировка в двумерном массиве" +
             static void DoTaskOleg() {
                  static void print(int[,] mass)
                 {
@@ -196,6 +220,135 @@ namespace ALG_LAB1 {
                      }
                  }
             }
+            
         }
     }
+    class GFG
+{
+    public const int RUN = 32;
+     
+    // This function sorts array from left index to
+    // to right index which is of size atmost RUN
+    public static void insertionSort(int[] arr, int left, int right)
+    {
+        for (int i = left + 1; i <= right; i++)
+        {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > temp)
+            {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+    }
+       
+    // merge function merges the sorted runs
+    public static void merge(int[] arr, int l, int m, int r)
+    {
+        // original array is broken in two parts
+        // left and right array
+        int len1 = m - l + 1, len2 = r - m;
+        int[] left = new int[len1];
+        int[] right = new int[len2];
+        for (int x = 0; x < len1; x++)
+            left[x] = arr[l + x];
+        for (int x = 0; x < len2; x++)
+            right[x] = arr[m + 1 + x];
+       
+        int i = 0;
+        int j = 0;
+        int k = l;
+       
+        // After comparing, we merge those two array
+        // in larger sub array
+        while (i < len1 && j < len2)
+        {
+            if (left[i] <= right[j])
+            {
+                arr[k] = left[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+       
+        // Copy remaining elements
+        // of left, if any
+        while (i < len1)
+        {
+            arr[k] = left[i];
+            k++;
+            i++;
+        }
+       
+        // Copy remaining element
+        // of right, if any
+        while (j < len2)
+        {
+            arr[k] = right[j];
+            k++;
+            j++;
+        }
+    }
+       
+    // Iterative Timsort function to sort the
+    // array[0...n-1] (similar to merge sort)
+    public static void timSort(int[] arr, int n)
+    {
+         
+        // Sort individual subarrays of size RUN
+        for (int i = 0; i < n; i+=RUN)
+            insertionSort(arr, i,
+                         Math.Min((i+RUN-1), (n-1)));
+       
+        // Start merging from size RUN (or 32).
+        // It will merge
+        // to form size 64, then
+        // 128, 256 and so on ....
+        for (int size = RUN; size < n;
+                                 size = 2*size)
+        {
+             
+            // Pick starting point of
+            // left sub array. We
+            // are going to merge
+            // arr[left..left+size-1]
+            // and arr[left+size, left+2*size-1]
+            // After every merge, we increase
+            // left by 2*size
+            for (int left = 0; left < n;
+                                  left += 2*size)
+            {
+                 
+                // Find ending point of left sub array
+                // mid+1 is starting point of
+                // right sub array
+                int mid = left + size - 1;
+                int right = Math.Min((left +
+                                    2*size - 1), (n-1));
+       
+                // Merge sub array arr[left.....mid] &
+                // arr[mid+1....right]
+                  if(mid < right)
+                    merge(arr, left, mid, right);
+            }
+        }
+    }
+       
+    // Utility function to print the Array
+    public static void printArray(int[] arr, int n)
+    {
+        for (int i = 0; i < n; i++)
+            Console.Write(arr[i] + " ");
+        Console.Write("\n");
+    }
+       
+    // Driver program to test above function
+}
 }
